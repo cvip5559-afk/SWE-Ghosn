db2413883abbac7d86a309b1115bee1a3effa243
ialeenz <cvip5559@gmail.com>
2026-04-19 02:18:29 +0300

    database

---
+++ database/ghosn_db.sql
@@ -0,0 +1,206 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 18, 2026 at 07:32 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ghosn_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity`
+--
+
+CREATE TABLE `activity` (
+  `Activity_ID` varchar(255) NOT NULL,
+  `ActivityDate` date NOT NULL,
+  `Status` enum('Pending','Completed') NOT NULL,
+  `Report_ID` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assign`
+--
+
+CREATE TABLE `assign` (
+  `Volunteer_ID` varchar(255) NOT NULL,
+  `Activity_ID` varchar(255) NOT NULL,
+  `ParticipationStatus` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+CREATE TABLE `location` (
+  `LocationID` varchar(255) NOT NULL,
+  `DistrictName` varchar(100) NOT NULL,
+  `Landmark` varchar(100) DEFAULT NULL,
+  `StreetName` varchar(100) NOT NULL,
+  `postalCode` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE `report` (
+  `ReportID` varchar(255) NOT NULL,
+  `Severity_Level` int(1) NOT NULL,
+  `Status` varchar(20) NOT NULL,
+  `Description` varchar(255) NOT NULL,
+  `Title` varchar(100) NOT NULL,
+  `CreatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `photo` varchar(500) DEFAULT NULL,
+  `resident_ID` varchar(255) NOT NULL,
+  `LocationID` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resident`
+--
+
+CREATE TABLE `resident` (
+  `resident_ID` varchar(255) NOT NULL,
+  `ResidentNeighbourhood` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `User_ID` varchar(255) NOT NULL,
+  `User_name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `volunteer`
+--
+
+CREATE TABLE `volunteer` (
+  `Volunteer_ID` varchar(255) NOT NULL,
+  `DateOfJoining` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activity`
+--
+ALTER TABLE `activity`
+  ADD PRIMARY KEY (`Activity_ID`),
+  ADD UNIQUE KEY `Report_ID` (`Report_ID`);
+
+--
+-- Indexes for table `assign`
+--
+ALTER TABLE `assign`
+  ADD PRIMARY KEY (`Volunteer_ID`,`Activity_ID`),
+  ADD KEY `Activity_ID` (`Activity_ID`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`LocationID`);
+
+--
+-- Indexes for table `report`
+--
+ALTER TABLE `report`
+  ADD PRIMARY KEY (`ReportID`),
+  ADD KEY `resident_ID` (`resident_ID`),
+  ADD KEY `LocationID` (`LocationID`);
+
+--
+-- Indexes for table `resident`
+--
+ALTER TABLE `resident`
+  ADD PRIMARY KEY (`resident_ID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`User_ID`);
+
+--
+-- Indexes for table `volunteer`
+--
+ALTER TABLE `volunteer`
+  ADD PRIMARY KEY (`Volunteer_ID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `activity`
+--
+ALTER TABLE `activity`
+  ADD CONSTRAINT `activity_ibfk_1` FOREIGN KEY (`Report_ID`) REFERENCES `report` (`ReportID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `assign`
+--
+ALTER TABLE `assign`
+  ADD CONSTRAINT `assign_ibfk_1` FOREIGN KEY (`Volunteer_ID`) REFERENCES `volunteer` (`Volunteer_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `assign_ibfk_2` FOREIGN KEY (`Activity_ID`) REFERENCES `activity` (`Activity_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `report`
+--
+ALTER TABLE `report`
+  ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`resident_ID`) REFERENCES `resident` (`resident_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `report_ibfk_2` FOREIGN KEY (`LocationID`) REFERENCES `location` (`LocationID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `resident`
+--
+ALTER TABLE `resident`
+  ADD CONSTRAINT `resident_ibfk_1` FOREIGN KEY (`resident_ID`) REFERENCES `user` (`User_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `volunteer`
+--
+ALTER TABLE `volunteer`
+  ADD CONSTRAINT `volunteer_ibfk_1` FOREIGN KEY (`Volunteer_ID`) REFERENCES `user` (`User_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
